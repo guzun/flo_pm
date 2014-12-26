a91425e6b43c3aca2fc73cff144e7a4a4e0acfa5
dmitryflo <job.verstka@gmail.com>
2014-12-26 18:28:28 +0200

    filter and info box

--- page-home.php
+++ page-home.php
@@ -2,10 +2,65 @@
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<div class="home-page">
 		<div class="team-category">
-			
+			<a href="#">designers</a>
+			<a href="#">developers</a>
 		</div>
 		<div class="active-projects">
 			<div class="developers-projects">
+				<?php
+					$args = array(
+						'post_type'   => 'projects',
+						'post_status' => 'publish',
+						'posts_per_page'  => -1,
+						'order'      => 'DESC',
+						'orderby'    => 'date',
+						'paged' => $paged,
+						'meta_query' => array(
+						'relation' => 'AND',
+						array(
+							'key'     => 'status',
+							'value'   => 'active',
+							'compare' => 'LIKE'
+						),
+						array(
+							'key'     => 'project_stage',
+							'value'   => 'development',
+							'compare' => 'LIKE'
+						)),
+					);
+					$wp_query = new WP_Query( $args );
+
+					$count_posts = $wp_query->post_count;
+
+					$user_id = $wp_query->posts[0]->ID;
+					$users = get_field('members', $user_id, $format_value);
+				?>
+				<?php if ($wp_query->have_posts()): ?>
+					<div class="header-box">
+						<div class="count-projects">
+							<?php echo $count_posts; ?>
+						</div>
+						<div class="change-grid">
+							<b class="line">line</b>
+							<b class="boxes">boxes</b>
+						</div>
+					</div>
+					<div class="items-projects">
+						<ul>
+							<?php while($wp_query->have_posts()): $wp_query->the_post(); ?>
+								<li>
+									<p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+									<div class="people">
+										<?php var_dump($users); ?>
+									</div>
+								</li>
+							<?php endwhile; ?>
+						</ul>
+					</div>
+				<?php endif ?>
+				<?php wp_reset_query(); ?>
+			</div>
+			<div class="designers-projects">
 				<?php
 					$args = array(
 						'post_type'   => 'projects',
@@ -14,32 +69,46 @@
 						'order'      => 'DESC',
 						'orderby'    => 'date',
 						'paged' => $paged,
-						// 'meta_key' => 'status',
-						// 'meta_value' => 'active'
 						'meta_query' => array(
 						'relation' => 'AND',
 						array(
 							'key'     => 'status',
 							'value'   => 'active',
-							'compare' => '='
+							'compare' => 'LIKE'
 						),
 						array(
 							'key'     => 'project_stage',
 							'value'   => 'design',
-							'compare' => '='
+							'compare' => 'LIKE'
 						)),
 					);
 					$wp_query = new WP_Query( $args );
+					$count_posts = $wp_query->post_count;
 				?>
 				<?php if ($wp_query->have_posts()): ?>
-					<?php while($wp_query->have_posts()): $wp_query->the_post(); ?>
-						<?php var_dump($args); ?>
-						<p><?php the_title(); ?></p>
-					<?php endwhile; ?>
+					<div class="header-box">
+						<div class="count-projects">
+							<?php echo $count_posts; ?>
+						</div>
+						<div class="change-grid">
+							<b class="line">line</b>
+							<b class="boxes">boxes</b>
+						</div>
+					</div>
+					<div class="item-projects">
+						<ul>
+							<?php while($wp_query->have_posts()): $wp_query->the_post(); ?>
+								<li>
+									<div class="info-project">
+										<p><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></p>
+									</div>
+								</li>
+							<?php endwhile; ?>
+						</ul>
+					</div>
 				<?php endif ?>
 				<?php wp_reset_query(); ?>
 			</div>
-			<div class="designers-projects"></div>
 		</div>
 	</div>
 <?php endwhile; else: ?>
