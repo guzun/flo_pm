a48d63b0ad2953ee0a4cfde8e77ae5bbb699a816
dmitryflo <job.verstka@gmail.com>
2014-12-26 17:11:11 +0200

    filter

--- page-home.php
+++ page-home.php
@@ -1,9 +1,47 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-	<section id="homepage" class="homepage">
-		homepage
-	</section>
-	<?php flo_part('pagefooter');?>
+	<div class="home-page">
+		<div class="team-category">
+			
+		</div>
+		<div class="active-projects">
+			<div class="developers-projects">
+				<?php
+					$args = array(
+						'post_type'   => 'projects',
+						'post_status' => 'publish',
+						'posts_per_page'  => 2,
+						'order'      => 'DESC',
+						'orderby'    => 'date',
+						'paged' => $paged,
+						// 'meta_key' => 'status',
+						// 'meta_value' => 'active'
+						'meta_query' => array(
+						'relation' => 'AND',
+						array(
+							'key'     => 'status',
+							'value'   => 'active',
+							'compare' => '='
+						),
+						array(
+							'key'     => 'project_stage',
+							'value'   => 'design',
+							'compare' => '='
+						)),
+					);
+					$wp_query = new WP_Query( $args );
+				?>
+				<?php if ($wp_query->have_posts()): ?>
+					<?php while($wp_query->have_posts()): $wp_query->the_post(); ?>
+						<?php var_dump($args); ?>
+						<p><?php the_title(); ?></p>
+					<?php endwhile; ?>
+				<?php endif ?>
+				<?php wp_reset_query(); ?>
+			</div>
+			<div class="designers-projects"></div>
+		</div>
+	</div>
 <?php endwhile; else: ?>
 	<?php flo_part('notfound')?>
 <?php endif; ?>
