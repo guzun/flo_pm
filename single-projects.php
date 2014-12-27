b4a510141e94c14cba9ed810299d841139ceb0d4
Oleg <Oleg@Olegs-MacBook-Pro.local>
2014-12-27 12:06:26 +0200

    add oleg.less file

--- single-projects.php
+++ single-projects.php
@@ -1,92 +1,92 @@
 <?php get_header(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 	<section>
-		<div class="status">
-			<?php $value = get_field('status'); ?>
-			<?php echo $value; ?>
+
+		<div class="left-side">
+			<a href="#">< back to projects</a>
+			<div class="status">
+				<?php $value = get_field('status'); ?>
+				<?php echo $value; ?>
+			</div>
+			<h3><?php the_title(); ?></h3>
+			<time pubdate="<?php the_time('c'); ?>"><?php the_time('F, Y');?></time>
+
+			<?php $peoples = get_field('members'); ?>
+			<?php if($peoples) : ?>
+				<ul class="people">
+					<?php foreach($peoples as $people) : ?>
+						<li>
+							<?php $url = wp_get_attachment_url( get_post_thumbnail_id($people->ID) ); ?>
+							<ul>
+								<?php $terms = wp_get_post_terms($people->ID, 'team-category');?>
+								<?php foreach ($terms as $term) : ?>
+									<li><?php echo $term->name; ?></li>
+								<?php endforeach; ?>
+							</ul>
+							<img src="<?php echo $url; ?>">
+							<a href="<?php echo get_permalink($people->ID); ?>"><?php echo get_the_title($people->ID) ?></a>
+							<span><?php echo get_field(); ?></span>
+						</li>
+					<?php endforeach; ?>
+				</ul>
+			<?php endif; ?>
 		</div>
 
-		<h3><?php the_title(); ?></h3>
-		<time pubdate="<?php the_time('c'); ?>"><?php the_time('F, Y');?></time>
-
-		<?php the_post_thumbnail(); ?>
-		<?php $terms = wp_get_post_terms($post->ID, 'project-technologies');?>
-		<?php if ($terms) : ?>
-			<ul class="category">
-				<?php foreach ($terms as $term) : ?>
-					<li><?php echo $term->name; ?></li>
-				<?php endforeach; ?>
-			</ul>
-		<?php endif; ?>
-
-		<ul class="files-section">
-			<?php if (have_rows('project_brand_files')) : ?>
-				<li>
-					<?php $title = get_field_object('project_brand_files'); ?>
-					<ul>
-						<h4><?php echo $title['label']; ?></h4>
-						<?php while(have_rows('project_brand_files')) : the_row(); ?>
-							<li>
-								<?php $x = explode('/', get_sub_field('brand_file'));?>
-								<a href="<?php get_sub_field('brand_file'); ?>"><?php echo($x[count($x)-1]); ?></a>
-							</li>
-						<?php endwhile; ?>
-					</ul>
-				</li>
+		<div class="right-side">
+			<?php $terms = wp_get_post_terms($post->ID, 'project-technologies');?>
+			<?php if ($terms) : ?>
+				<ul class="category">
+					<?php foreach ($terms as $term) : ?>
+						<li><?php echo $term->name; ?></li>
+					<?php endforeach; ?>
+				</ul>
 			<?php endif; ?>
-			<?php if (have_rows('project_design_source_files')) : ?>
-				<li>
-					<?php $title = get_field_object('project_design_source_files'); ?>
-					<ul>
-						<h4><?php echo $title['label']; ?></h4>
-						<?php while(have_rows('project_design_source_files')) : the_row(); ?>
-							<li>
-								<?php $x = explode('/', get_sub_field('source_files'));?>
-								<a href="<?php get_sub_field('brand_file'); ?>"><?php echo($x[count($x)-1]); ?></a>
-							</li>
-						<?php endwhile; ?>
-					</ul>
-				</li>
-			<?php endif; ?>
-			<?php if (have_rows('project_scope_of_work')) : ?>
-				<li>
-					<?php $title = get_field_object('project_scope_of_work'); ?>
-					<ul>
-						<h4><?php echo $title['label']; ?></h4>
-						<?php while(have_rows('project_scope_of_work')) : the_row(); ?>
-							<li>
-								<?php $x = explode('/', get_sub_field('scope_file'));?>
-								<a href="<?php get_sub_field('brand_file'); ?>"><?php echo($x[count($x)-1]); ?></a>
-							</li>
-						<?php endwhile; ?>
-					</ul>
-				</li>
-			<?php endif; ?>
-		</ul>
-
-		<?php $peoples = get_field('members'); ?>
-		<?php if($peoples) : ?>
-			<ul class="people">
-				<?php foreach($peoples as $people) : ?>
+			<ul class="files-section">
+				<?php if (have_rows('project_brand_files')) : ?>
 					<li>
-						<?php $url = wp_get_attachment_url( get_post_thumbnail_id($people->ID) ); ?>
+						<?php $title = get_field_object('project_brand_files'); ?>
 						<ul>
-							<?php $terms = wp_get_post_terms($people->ID, 'team-category');?>
-							<?php foreach ($terms as $term) : ?>
-								<li><?php echo $term->name; ?></li>
-							<?php endforeach; ?>
+							<h4><?php echo $title['label']; ?></h4>
+							<?php while(have_rows('project_brand_files')) : the_row(); ?>
+								<li>
+									<?php $x = explode('/', get_sub_field('brand_file'));?>
+									<a href="<?php get_sub_field('brand_file'); ?>"><?php echo($x[count($x)-1]); ?></a>
+								</li>
+							<?php endwhile; ?>
 						</ul>
-						<img src="<?php echo $url; ?>">
-						<a href="<?php echo get_permalink($people->ID); ?>"><?php echo get_the_title($people->ID) ?></a>
-						<span><?php echo get_field(); ?></span>
 					</li>
-				<?php endforeach; ?>
+				<?php endif; ?>
+				<?php if (have_rows('project_design_source_files')) : ?>
+					<li>
+						<?php $title = get_field_object('project_design_source_files'); ?>
+						<ul>
+							<h4><?php echo $title['label']; ?></h4>
+							<?php while(have_rows('project_design_source_files')) : the_row(); ?>
+								<li>
+									<?php $x = explode('/', get_sub_field('source_files'));?>
+									<a href="<?php get_sub_field('brand_file'); ?>"><?php echo($x[count($x)-1]); ?></a>
+								</li>
+							<?php endwhile; ?>
+						</ul>
+					</li>
+				<?php endif; ?>
+				<?php if (have_rows('project_scope_of_work')) : ?>
+					<li>
+						<?php $title = get_field_object('project_scope_of_work'); ?>
+						<ul>
+							<h4><?php echo $title['label']; ?></h4>
+							<?php while(have_rows('project_scope_of_work')) : the_row(); ?>
+								<li>
+									<?php $x = explode('/', get_sub_field('scope_file'));?>
+									<a href="<?php get_sub_field('brand_file'); ?>"><?php echo($x[count($x)-1]); ?></a>
+								</li>
+							<?php endwhile; ?>
+						</ul>
+					</li>
+				<?php endif; ?>
 			</ul>
-		<?php endif; ?>
-		
-		
-
-		<div class="story"><?php the_content(); ?></div>
+			<div class="story"><?php the_content(); ?></div>
+		</div>
 	</section>
 <?php endwhile; else: ?>
 	<?php flo_part('notfound')?>
