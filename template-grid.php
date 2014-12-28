2fb9be322b50b41c4c0578903833f817520d71b0
alecsandre <diacomd@gmail.com>
2014-12-28 15:35:43 +0200

    FloPM - added people page

--- template-grid.php
+++ template-grid.php
@@ -5,17 +5,150 @@
 get_header();
 ?>
 
-<secton class="projects-list list-view">
-	
-	<div class="options">
+<div class="options">
+
+	<div class="option-items">
+		Option items here
+	</div>
 		
-		<div class="result-info">
-			<a href="#" class="toggle-view show-list" data-view="list-view"><i></i></a>
-			<a href="#" class="toggle-view show-grid" data-view="grid-view"><i></i></a>
-			<span class="projects-num">23 Projects</span>
+	<div class="result-info">
+		<a href="#" class="toggle-view show-list" data-view="list-view"><i></i></a>
+		<a href="#" class="toggle-view show-grid" data-view="grid-view"><i></i></a>
+		<span class="projects-num">23 Projects</span>
+	</div>
+
+</div>
+
+<section class="result-list list-view">
+
+	<div class="results">
+		
+		<!--BOF - This is the top row of the list. Has different marckup -->
+		<div class="result-header">
+			<div class="result-item-inner">
+				<div class="result-meta">
+					<div class="result-meta-inner">
+						<div class="name">Project Name</div>
+						<div class="category">Category</div>
+						<div class="member">Member</div>
+						<div class="date">Date</div>
+						<div class="status open">Status</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- EOF - top row -->
+		
+		<div class="result-item">
+			<div class="result-item-inner">
+				<div class="result-image">
+					<img src="http://lorempixel.com/500/500" alt="" />
+				</div>
+				<div class="result-meta">
+					<a href="#" class="result-item-link"></a>
+					<div class="result-meta-inner">
+						<div class="name"><a href="#">Project Name</a></div>
+						<div class="category"><a href="#">Category</a></div>
+						<div class="member">
+							<a href="#">Member 1,</a>
+							<a href="#">Member 2</a>
+						</div>
+						<div class="date">27.12.2014</div>
+						<div class="status open"><span></span> Active</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="result-item">
+			<div class="result-item-inner">
+				<div class="result-image">
+					<img src="http://lorempixel.com/500/500" alt="" />
+				</div>
+				<div class="result-meta">
+					<a href="#" class="result-item-link"></a>
+					<div class="result-meta-inner">
+						<div class="name"><a href="#">Project Name</a></div>
+						<div class="category"><a href="#">Category</a></div>
+						<div class="member">
+							<a href="#">Member 1,</a>
+							<a href="#">Member 2</a>
+						</div>
+						<div class="date">27.12.2014</div>
+						<div class="status closed"><span></span> Closed</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="result-item">
+			<div class="result-item-inner">
+				<div class="result-image">
+					<img src="http://lorempixel.com/500/500" alt="" />
+				</div>
+				<div class="result-meta">
+					<a href="#" class="result-item-link"></a>
+					<div class="result-meta-inner">
+						<div class="name"><a href="#">Project Name</a></div>
+						<div class="category"><a href="#">Category</a></div>
+						<div class="member">
+							<a href="#">Member 1,</a>
+							<a href="#">Member 2</a>
+						</div>
+						<div class="date">27.12.2014</div>
+						<div class="status closed"><span></span> Closed</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="result-item">
+			<div class="result-item-inner">
+				<div class="result-image">
+					<img src="http://lorempixel.com/500/500" alt="" />
+				</div>
+				<div class="result-meta">
+					<a href="#" class="result-item-link"></a>
+					<div class="result-meta-inner">
+						<div class="name"><a href="#">Project Name</a></div>
+						<div class="category"><a href="#">Category</a></div>
+						<div class="member">
+							<a href="#">Member 1,</a>
+							<a href="#">Member 2</a>
+						</div>
+						<div class="date">27.12.2014</div>
+						<div class="status closed"><span></span> Closed</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="result-item">
+			<div class="result-item-inner">
+				<div class="result-image">
+					<img src="http://lorempixel.com/500/500" alt="" />
+				</div>
+				<div class="result-meta">
+					<a href="#" class="result-item-link"></a>
+					<div class="result-meta-inner">
+						<div class="name"><a href="#">Project Name</a></div>
+						<div class="category"><a href="#">Category</a></div>
+						<div class="member">
+							<a href="#">Member 1,</a>
+							<a href="#">Member 2</a>
+						</div>
+						<div class="date">27.12.2014</div>
+						<div class="status closed"><span></span> Closed</div>
+					</div>
+				</div>
+			</div>
 		</div>
 
 	</div>
+
+</section>
+
+<section class="result-list grid-view">
 
 	<div class="results">
 		
@@ -34,7 +167,7 @@ get_header();
 							<a href="#">Member 2</a>
 						</div>
 						<div class="date">27.12.2014</div>
-						<div class="status open"><span></span></div>
+						<div class="status open"><span></span> Active</div>
 					</div>
 				</div>
 			</div>
@@ -55,7 +188,7 @@ get_header();
 							<a href="#">Member 2</a>
 						</div>
 						<div class="date">27.12.2014</div>
-						<div class="status closed"><span></span></div>
+						<div class="status closed"><span></span> Closed</div>
 					</div>
 				</div>
 			</div>
@@ -76,7 +209,7 @@ get_header();
 							<a href="#">Member 2</a>
 						</div>
 						<div class="date">27.12.2014</div>
-						<div class="status closed"><span></span></div>
+						<div class="status closed"><span></span> Closed</div>
 					</div>
 				</div>
 			</div>
@@ -97,7 +230,7 @@ get_header();
 							<a href="#">Member 2</a>
 						</div>
 						<div class="date">27.12.2014</div>
-						<div class="status closed"><span></span></div>
+						<div class="status closed"><span></span> Closed</div>
 					</div>
 				</div>
 			</div>
@@ -118,7 +251,7 @@ get_header();
 							<a href="#">Member 2</a>
 						</div>
 						<div class="date">27.12.2014</div>
-						<div class="status closed"><span></span></div>
+						<div class="status closed"><span></span> Closed</div>
 					</div>
 				</div>
 			</div>
@@ -126,130 +259,7 @@ get_header();
 
 	</div>
 
-</secton>
-
-<secton class="projects-list grid-view">
-	
-	<div class="options">
-		
-		<div class="result-info">
-			<a href="#" class="toggle-view show-list" data-view="list-view"><i></i></a>
-			<a href="#" class="toggle-view show-grid" data-view="grid-view"><i></i></a>
-			<span class="projects-num">23 Projects</span>
-		</div>
-
-	</div>
-
-	<div class="results">
-		
-		<div class="result-item">
-			<div class="result-item-inner">
-				<div class="result-image">
-					<img src="http://lorempixel.com/500/500" alt="" />
-				</div>
-				<div class="result-meta">
-					<a href="#" class="result-item-link"></a>
-					<div class="result-meta-inner">
-						<div class="name"><a href="#">Project Name</a></div>
-						<div class="category"><a href="#">Category</a></div>
-						<div class="member">
-							<a href="#">Member 1,</a>
-							<a href="#">Member 2</a>
-						</div>
-						<div class="date">27.12.2014</div>
-						<div class="status open"><span></span></div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="result-item">
-			<div class="result-item-inner">
-				<div class="result-image">
-					<img src="http://lorempixel.com/500/500" alt="" />
-				</div>
-				<div class="result-meta">
-					<a href="#" class="result-item-link"></a>
-					<div class="result-meta-inner">
-						<div class="name"><a href="#">Project Name</a></div>
-						<div class="category"><a href="#">Category</a></div>
-						<div class="member">
-							<a href="#">Member 1,</a>
-							<a href="#">Member 2</a>
-						</div>
-						<div class="date">27.12.2014</div>
-						<div class="status closed"><span></span></div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="result-item">
-			<div class="result-item-inner">
-				<div class="result-image">
-					<img src="http://lorempixel.com/500/500" alt="" />
-				</div>
-				<div class="result-meta">
-					<a href="#" class="result-item-link"></a>
-					<div class="result-meta-inner">
-						<div class="name"><a href="#">Project Name</a></div>
-						<div class="category"><a href="#">Category</a></div>
-						<div class="member">
-							<a href="#">Member 1,</a>
-							<a href="#">Member 2</a>
-						</div>
-						<div class="date">27.12.2014</div>
-						<div class="status closed"><span></span></div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="result-item">
-			<div class="result-item-inner">
-				<div class="result-image">
-					<img src="http://lorempixel.com/500/500" alt="" />
-				</div>
-				<div class="result-meta">
-					<a href="#" class="result-item-link"></a>
-					<div class="result-meta-inner">
-						<div class="name"><a href="#">Project Name</a></div>
-						<div class="category"><a href="#">Category</a></div>
-						<div class="member">
-							<a href="#">Member 1,</a>
-							<a href="#">Member 2</a>
-						</div>
-						<div class="date">27.12.2014</div>
-						<div class="status closed"><span></span></div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<div class="result-item">
-			<div class="result-item-inner">
-				<div class="result-image">
-					<img src="http://lorempixel.com/500/500" alt="" />
-				</div>
-				<div class="result-meta">
-					<a href="#" class="result-item-link"></a>
-					<div class="result-meta-inner">
-						<div class="name"><a href="#">Project Name</a></div>
-						<div class="category"><a href="#">Category</a></div>
-						<div class="member">
-							<a href="#">Member 1,</a>
-							<a href="#">Member 2</a>
-						</div>
-						<div class="date">27.12.2014</div>
-						<div class="status closed"><span></span></div>
-					</div>
-				</div>
-			</div>
-		</div>
-
-	</div>
-
-</secton>
+</section>
 
 <?php
 get_footer(); 
---
+++ template-people.php
@@ -0,0 +1,134 @@
+<?php 
+/**
+ * Template Name: Template People
+ */
+get_header();
+?>
+
+<div class="options">
+
+	<div class="option-items">
+		Option items here
+	</div>
+		
+	<div class="result-info">
+		<span class="projects-num">23 People</span>
+	</div>
+
+</div>
+
+<section class="members-list">
+
+	<div class="members-list-inner">
+		
+		<div class="member-item">
+			<div class="member-item-inner">
+				<a href="#" class="member-item-link"></a>
+				<div class="member-image">
+					<div class="hover"></div>
+					<img src="http://lorempixel.com/500/500/people" alt="" />
+				</div>
+				<div class="member-meta">
+					<span class="member-name">John Doe</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="member-item">
+			<div class="member-item-inner">
+				<a href="#" class="member-item-link"></a>
+				<div class="member-image">
+					<div class="hover"></div>
+					<img src="http://lorempixel.com/500/500/people" alt="" />
+				</div>
+				<div class="member-meta">
+					<span class="member-name">John Doe</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="member-item">
+			<div class="member-item-inner">
+				<a href="#" class="member-item-link"></a>
+				<div class="member-image">
+					<div class="hover"></div>
+					<img src="http://lorempixel.com/500/500/people" alt="" />
+				</div>
+				<div class="member-meta">
+					<span class="member-name">John Doe</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="member-item">
+			<div class="member-item-inner">
+				<div class="member-image">
+					<a href="#">
+						<img src="http://lorempixel.com/500/500/people" alt="" />
+					</a>
+				</div>
+				<div class="member-meta">
+					<span class="member-name">John Doe</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="member-item">
+			<div class="member-item-inner">
+				<div class="member-image">
+					<a href="#">
+						<img src="http://lorempixel.com/500/500/people" alt="" />
+					</a>
+				</div>
+				<div class="member-meta">
+					<span class="member-name">John Doe</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="member-item">
+			<div class="member-item-inner">
+				<div class="member-image">
+					<a href="#">
+						<img src="http://lorempixel.com/500/500/people" alt="" />
+					</a>
+				</div>
+				<div class="member-meta">
+					<span class="member-name">John Doe</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="member-item">
+			<div class="member-item-inner">
+				<div class="member-image">
+					<a href="#">
+						<img src="http://lorempixel.com/500/500/people" alt="" />
+					</a>
+				</div>
+				<div class="member-meta">
+					<span class="member-name">John Doe</span>
+				</div>
+			</div>
+		</div>
+
+		<div class="member-item">
+			<div class="member-item-inner">
+				<div class="member-image">
+					<a href="#">
+						<img src="http://lorempixel.com/500/500/people" alt="" />
+					</a>
+				</div>
+				<div class="member-meta">
+					<span class="member-name">John Doe</span>
+				</div>
+			</div>
+		</div>
+
+	</div>
+
+</section>
+
+<?php 
+get_footer();
+?>
